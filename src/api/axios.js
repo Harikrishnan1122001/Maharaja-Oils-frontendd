a1e3a23ba7f78ec570f6ad66ffd5e654adf6269c
Harikrishnan1122001 <hk1396897@gmail.com>
2026-09-11 13:16:50 +0530

    new code

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "https://maharaja-oils-backend.vercel.app/api",
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("mo_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
